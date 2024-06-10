c741f4a07cd7ce5f4b9553f5b07475c2a9131989
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2024-06-10 22:56:05 +0530

    Changed terminal to nvchadTerm

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,15 +1,34 @@
 return {
-  "akinsho/toggleterm.nvim",
-  config = function()
-    require("toggleterm").setup({
-      open_mapping = "<Leader>v",
-      direction = "horizontal",
-      border= "none"
-    })
+	"NvChad/nvterm",
+	config = function()
+		require("nvterm").setup({
+			terminals = {
+				horizontal = {
+					location = "rightbelow",
+					split_ratio = .3,
+				},
+				vertical = {
+					location = "rightbelow",
+					split_ratio = .000001,
+				},
 
-    vim.api.nvim_set_keymap(
-      'n', '<Leader>h', '<Cmd>exe v:count1 . "ToggleTerm direction=vertical size=50"<CR>',
-      { noremap = true, silent = true }
-    )
- end,
+				behavior = {
+					autoclose_on_quit = {
+						enabled = true,
+						confirm = true,
+					},
+					close_on_exit = true,
+					auto_insert = true,
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>h", function()
+			require("nvterm.terminal").toggle("horizontal")
+		end, { noremap = true, silent = true, desc = "Toggle Horizontal Terminal" })
+
+		vim.keymap.set("n", "<leader>v", function()
+			require("nvterm.terminal").toggle("vertical")
+		end, { noremap = true, silent = true, desc = "Toggle Vertical Terminal" })
+	end,
 }
