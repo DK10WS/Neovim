3164d9ea52baf497e9c95ef23ca135c13e125f4e
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2024-06-18 05:14:32 +0530

    fixed python lsp to give functionality for libraries

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,7 +20,7 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.pylsp.setup({ capabilities = capabilities })
 			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 			lspconfig.clangd.setup({ capabilities = capabilities })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
