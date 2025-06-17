f49b6f510966a0193d3ac6bcf34fab901e594bfb
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2025-06-17 20:14:14 +0530

    Fixed lps error showing and added code actions lightbulb

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,79 @@
 return {
+  -- Mason: LSP installer
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.pylsp.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+  -- Auto install LSPs with Mason
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+
+  -- LSP Configurations
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Setup LSPs
+      local servers = {
+        lua_ls = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                pylint = { enabled = false },
+                mccabe = { enabled = false },
+              },
+            },
+          },
+        },
+        rust_analyzer = {},
+        clangd = {},
+      }
+
+      for server, opts in pairs(servers) do
+        opts.capabilities = capabilities
+        lspconfig[server].setup(opts)
+      end
+
+      -- Diagnostic floating window on CursorHold
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          source = "always",
+        },
+      })
+
+      vim.api.nvim_create_autocmd("CursorHold", {
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false })
+        end,
+      })
+
+      -- Speed up CursorHold event
+      vim.o.updatetime = 250
+
+      -- Keymaps for LSP features
+      local map = vim.keymap.set
+      map("n", "K", vim.lsp.buf.hover, {})
+      map("n", "<leader>gd", vim.lsp.buf.definition, {})
+      map("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
